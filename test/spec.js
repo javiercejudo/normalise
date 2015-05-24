8730ec784d59a0b753a4b882fcbaebd84b9cb11b
Javier Cejudo <javier@javiercejudo.com>
2015-05-24 15:24:41 +1000

    feat(precision): add support for arbitrary precision with big.js

--- test/spec.js
+++ test/spec.js
@@ -5,6 +5,8 @@
 var should = require('should');
 var sinon = require('sinon');
 var rescaleUtil = require('rescale-util');
+var big = require('big.js');
+var arbitraryPrecision = require('rescale-arbitrary-precision');
 var normalise = require('../src/normalise.js').normalise;
 
 describe('normalising', function() {
@@ -48,9 +50,40 @@ describe('normalising', function() {
       isValidScaleStub.restore();
     });
 
-    it('should normalise data', function() {
-      normalise(2.5, [0, 5]).should.be.exactly(0.5);
-      normalise(-3, [-5, 1]).should.be.exactly(1/3);
+    describe('when big.js is available', function() {
+      var hasArbitraryPrecisionStub;
+
+      beforeEach(function() {
+        hasArbitraryPrecisionStub = sinon.stub(arbitraryPrecision, 'isAvailable');
+        hasArbitraryPrecisionStub.returns(true);
+      });
+
+      afterEach(function() {
+        hasArbitraryPrecisionStub.restore();
+      });
+
+      it('should work with arbitrary precision', function() {
+        normalise(0.4, [0.3, 0.5]).should.eql(big(0.5));
+        normalise(-3, [-5, 1]).should.eql(big(1).div(3));
+      });
+    });
+
+    describe('when big.js is unavailable', function() {
+      var hasArbitraryPrecisionStub;
+
+      beforeEach(function() {
+        hasArbitraryPrecisionStub = sinon.stub(arbitraryPrecision, 'isAvailable');
+        hasArbitraryPrecisionStub.returns(false);
+      });
+
+      afterEach(function() {
+        hasArbitraryPrecisionStub.restore();
+      });
+
+      it('should work with floating-point numbers', function() {
+        normalise(0.4, [0.3, 0.5]).should.be.exactly(0.5000000000000001);
+        normalise(-3, [-5, 1]).should.be.exactly(1/3);
+      });
     });
   });
 
