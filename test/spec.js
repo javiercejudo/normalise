7c644d9795ff04f64ca35fcd4eb78f12137d714e
Javier Cejudo <javier@javiercejudo.com>
2015-08-19 22:04:49 +1000

    feat(decimal): the factory takes decimal and normalise returns decimal

--- test/spec.js
+++ test/spec.js
@@ -4,36 +4,54 @@
 
 require('should');
 
-var normaliseFactory = require('../src/normalise');
-var bigjsAdapter = require('bigjs-adapter')
+var arbitraryPrecision = require('linear-arbitrary-precision');
+var bigjsAdapter = require('bigjs-adapter');
 var floatingAdapter = require('floating-adapter');
+var normaliseFactory = require('../src/normalise');
 
 describe('normalising', function() {
   describe('without a scale', function() {
-    var normalise = normaliseFactory(floatingAdapter).normalise;
+    var Decimal = arbitraryPrecision(floatingAdapter);
+    var normalise = normaliseFactory(Decimal).normalise;
 
     it('should be the identity', function() {
-      normalise(42).should.be.exactly(42);
-      normalise(Math.E).should.be.exactly(Math.E);
+      normalise(42).val().val().should.be.exactly(42);
+      normalise(Math.E).val().val().should.be.exactly(Math.E);
     });
   });
 
   describe('with valid scales', function() {
+    describe('should support', function() {
+      var Decimal = arbitraryPrecision(floatingAdapter);
+      var normalise = normaliseFactory(Decimal).normalise;
+
+      it('native numbers', function() {
+        normalise(0.4, [0.3, 0.5]).val().val().should.be.exactly(0.5000000000000001);
+      });
+
+      it('as well as Decimal numbers', function() {
+        normalise(new Decimal('0.4'), [new Decimal('0.3'), new Decimal('0.5')])
+          .val().val().should.be.exactly(0.5000000000000001);
+      });
+    });
+
     describe('when arbitrary precision is available', function() {
-      var normalise = normaliseFactory(bigjsAdapter).normalise;
+      var Decimal = arbitraryPrecision(bigjsAdapter);
+      var normalise = normaliseFactory(Decimal).normalise;
 
       it('should work with arbitrary precision', function() {
-        normalise(0.4, [0.3, 0.5]).should.be.exactly(1/2);
-        normalise(-3, [-5, 1]).should.be.exactly(1/3);
+        normalise(0.4, [0.3, 0.5]).val().eq(new Decimal('0.5')).should.be.exactly(true);
+        normalise(-3, [-5, 1]).val().eq(new Decimal('1').div(new Decimal('3'))).should.be.exactly(true);
       });
     });
 
     describe('when arbitrary precision is unavailable', function() {
-      var normalise = normaliseFactory(floatingAdapter).normalise;
+      var Decimal = arbitraryPrecision(floatingAdapter);
+      var normalise = normaliseFactory(Decimal).normalise;
 
       it('should work with floating-point numbers', function() {
-        normalise(0.4, [0.3, 0.5]).should.be.exactly(0.5000000000000001);
-        normalise(-3, [-5, 1]).should.be.exactly(1/3);
+        normalise(0.4, [0.3, 0.5]).val().val().should.be.exactly(0.5000000000000001);
+        normalise(-3, [-5, 1]).val().val().should.be.exactly(1/3);
       });
     });
   });
