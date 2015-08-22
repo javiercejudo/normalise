bf3537e7af5e47d3bf2a195d45968a06e55067c8
Javier Cejudo <javier@javiercejudo.com>
2015-08-22 15:33:34 +1000

    chore: simplify tests

--- test/spec.js
+++ test/spec.js
@@ -4,7 +4,7 @@
 
 require('should');
 
-var arbitraryPrecision = require('linear-arbitrary-precision');
+var arbitraryPrecision = require('arbitrary-precision');
 var bigjsAdapter = require('bigjs-adapter');
 var floatingAdapter = require('floating-adapter');
 var normaliseFactory = require('../src/normalise');
@@ -26,12 +26,12 @@ describe('normalising', function() {
       var normalise = normaliseFactory(Decimal).normalise;
 
       it('native numbers', function() {
-        normalise(0.4, [0.3, 0.5]).val().val().should.be.exactly(0.5000000000000001);
+        normalise(0.4, [0.3, 0.5]).equals(new Decimal('0.5000000000000001')).should.be.exactly(true);
       });
 
       it('as well as Decimal numbers', function() {
         normalise(new Decimal('0.4'), [new Decimal('0.3'), new Decimal('0.5')])
-          .val().val().should.be.exactly(0.5000000000000001);
+          .equals(new Decimal('0.5000000000000001')).should.be.exactly(true);
       });
     });
 
@@ -40,8 +40,8 @@ describe('normalising', function() {
       var normalise = normaliseFactory(Decimal).normalise;
 
       it('should work with arbitrary precision', function() {
-        normalise(0.4, [0.3, 0.5]).val().eq(new Decimal('0.5')).should.be.exactly(true);
-        normalise(-3, [-5, 1]).val().eq(new Decimal('1').div(new Decimal('3'))).should.be.exactly(true);
+        normalise(0.4, [0.3, 0.5]).equals(new Decimal('0.5')).should.be.exactly(true);
+        normalise(-3, [-5, 1]).equals(new Decimal('1').div(new Decimal('3'))).should.be.exactly(true);
       });
     });
 
@@ -50,8 +50,10 @@ describe('normalising', function() {
       var normalise = normaliseFactory(Decimal).normalise;
 
       it('should work with floating-point numbers', function() {
-        normalise(0.4, [0.3, 0.5]).val().val().should.be.exactly(0.5000000000000001);
-        normalise(-3, [-5, 1]).val().val().should.be.exactly(1/3);
+        normalise(0.4, [0.3, 0.5]).equals(new Decimal('0.5000000000000001')).should.be.exactly(true);
+
+        normalise(-3, [-5, 1]).equals(new Decimal('1').div(new Decimal('3')))
+          .should.be.exactly(true);
       });
     });
   });
